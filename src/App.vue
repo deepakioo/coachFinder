feccd321bdf26f3ce297c865c1e45cc2d9b153d9
deepakiooLabs <60341710+deepakiooLabs@users.noreply.github.com>
2021-01-28 09:46:33 +0530

    folder structurre added

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+   <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+   @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
+html, body {
+  font-family: 'Roboto', sans-serif;
+}
+
+#app {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ src/pages/coaches/CoachRegistraion.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>Register Coach</h3>
+</template>
