6ef1a46a211157ec8e467fd343a92b9481539550
deepakiooLabs <60341710+deepakiooLabs@users.noreply.github.com>
2021-02-02 19:26:19 +0530

    adding coach data in vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,2 +1,14 @@
 
-export default {}
+export default {
+    registerCoach(context,data){
+        const coachData = {
+            id:context.rootGetters.userId,
+            firstName:data.first,
+            lastName:data.last,
+            areas:data.areas,
+            description:data.description,
+            hourlyRate:data.rate
+        }
+        context.commit('registerCoach',coachData)
+    }
+}
